8260ebdc1d094840a757dd55f74b15365c94339f
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-05-09 00:45:43 -0500

    building the questions and answers code into the buttons

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 var prepareQuiz = document.querySelector("#leggo");
 var timerEl = document.getElementById("timer");
-var timer = 75;  
+var timer = 75;
+
+const randQuestions, currentQuestionIndex
+
+const questions = [
+    {
+        question: 'what is the first letter of the alphabet?',
+        answers: [
+            { text: 'A', correct: true },
+            { text: 'B', correct: false },
+            { text: 'C', correct: false },
+            { text: 'D', correct: false },
+        ]
+    }
+]
+
+function startQuiz() {
+    console.log('Started')
+    startButton.classList.add('hide')
+    randQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    quizboxElement.classList.remove('hide')
+    questionHopper()
+}
+
+function displayQuestion(question) {
+    
+}
+
+function chooseAnswer() {
+
+}
+
+function questionHopper() {
+
+}
 
 prepareQuiz.addEventListener("click", function() {
     var createTimer = setInterval(function () {
