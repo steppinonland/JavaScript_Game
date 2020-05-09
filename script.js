d943c765f3bdcb52d5c4202208edcd0f564edc41
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-05-08 22:47:22 -0500

    got timer to start

---
+++ script.js
@@ -0,0 +1,19 @@
+var prepareQuiz = document.querySelector("#leggo");
+var timerEl = document.getElementById("timer");
+var timer = 75;  
+
+prepareQuiz.addEventListener("click", function() {
+    var createTimer = setInterval(function () {
+        // runs every second
+        timer--;
+
+        //if timer runs out
+        if(timer === 0)
+ {
+     clearInterval(createTimer);
+     completeQuiz();
+ }    
+// update the timer on page 
+document.getElementById("timer").innerHTML = timer + "seconds";
+    }, 1000); 
+}); 
